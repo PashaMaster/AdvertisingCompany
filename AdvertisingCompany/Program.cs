abaa8d60f7d1383815a4cb84d6d5b7aa44ed7317
Compaq <Compaq@WIN-TKCQ9HTD1G1>
2017-11-13 23:24:26 +0200

    Add Initializer

--- AdvertisingCompany/Program.cs
+++ AdvertisingCompany/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using AdvertisingCompany.Models;
 
 namespace AdvertisingCompany
 {
@@ -14,7 +16,24 @@ namespace AdvertisingCompany
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<OrderContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
